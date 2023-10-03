de4a742edce290a2fb0a282d7f7de3c74dbc5752
mateoagudo <mateo.agudo@ucuenca.edu.ec>
2023-10-02 21:32:21 -0500

    la pelota rebota con la misma aceleración mientras cae y sube

--- sketch.js
+++ sketch.js
@@ -1,48 +1,40 @@
-let fondo, posX, posY, diam, rad, esp, margen, piso, velY, velX;
+let fondo, posX, posY, diam, rad, esp, margen, piso, velX;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   posX = windowWidth / 2;
-  posY = 100;
+  posY = windowHeight * 0.2;
   circulo = 255;
-  diam = 50;
+  diam = random(10, 50);
   rad = diam / 2;
   rectMode(CENTER);
-  esp = 34;
-  margen = 50;
+  esp = 50;
+  margen = 40;
   velY = 10;
-  velX = 5;
+  acel = 0.98;
   piso = windowHeight - margen - esp / 2;
 }
 
 function draw() {
-  background(0, 200, 150, 10);
+  background(0, 200, 150, 70);
+
+  update();
+
   noStroke();
-
-  posX += velX;
-  if (posX > windowWidth * 0.9 - rad) {
-    velX *= -1;
-  }
-
-  if (posX < windowWidth * 0.1 + rad) {
-    velX *= -1;
-  }
-
-  posY += velY;
-  if (posY > piso - rad) {
-    velY *= -1;
-
-    print("se choco");
-  }
-
-  if (posY < 100) {
-    velY *= -1;
-  }
-
   fill(circulo);
   circle(posX, posY, diam);
 
-  fill(255, 0, 0);
-  rect(windowWidth / 2, 100 - esp / 2 - rad, windowWidth * 0.8, esp);
-  rect(windowWidth / 2, windowHeight - margen, windowWidth * 0.8, esp);
+  fill(100, 50, 0);
+  rect(windowWidth / 2, windowHeight - margen, windowWidth, esp);
+}
+
+function update() {
+  velY += acel;
+  posY += velY;
+
+  if (posY >= piso - rad) {
+    print("se choco");
+    velY *= -1;
+    posY += velY;
+  }
 }
